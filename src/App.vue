a95984b6b4af8fcfec11f0b9f5bf623ff9aaa4b4
Philip Zimmermann <philip.zimmermann@freenet.de>
2023-05-08 18:22:53 +0200

    add getter favs add buttons to show all and only favs

--- src/App.vue
+++ src/App.vue
@@ -4,8 +4,21 @@
       <img src="./assets/pinia-logo.svg" alt="pinia logo" />
       <h1>Pinia Tasks</h1>
     </header>
-    <div class="task-list">
+
+    <nav class="filter">
+      <button @click="filter = 'all'">All tasks</button>
+      <button @click="filter = 'favs'">Fav tasks</button>
+    </nav>
+
+    <div class="task-list" v-if="filter === 'all'">
+      <p>all tasks</p>
       <div v-for="task in taskStore.tasks">
+        <TaskDetails :task="task" />
+      </div>
+    </div>
+    <div class="task-list" v-if="filter === 'favs'">
+      <p>fav tasks</p>
+      <div v-for="task in taskStore.favs">
         <TaskDetails :task="task" />
       </div>
     </div>
@@ -13,6 +26,7 @@
 </template>
 
 <script>
+import { ref } from "vue";
 import TaskDetails from "./components/TaskDetails.vue";
 import { useTaskStore } from "./store/TaskStore";
 
@@ -23,7 +37,9 @@ export default {
   setup() {
     const taskStore = useTaskStore();
 
-    return { taskStore };
+    const filter = ref("all");
+
+    return { taskStore, filter };
   },
 };
 </script>
